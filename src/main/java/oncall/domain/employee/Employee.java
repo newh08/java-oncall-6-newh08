c16b5ac3a13fb64f30df5e1df0eb6f98fe988293
newh08 <newh08@gmail.com>
2023-12-16 17:55:58 +0900

    ✨FEAT: 예외 상황 추가 구현

--- src/main/java/oncall/domain/employee/Employee.java
+++ src/main/java/oncall/domain/employee/Employee.java
@@ -1,16 +1,25 @@
 package oncall.domain.employee;
 
 import java.util.Objects;
+import oncall.util.exception.ErrorMessage;
+import oncall.util.exception.MyIllegalArgumentException;
 
 public class Employee {
     private final String name;
 
     public Employee(String name) {
+        validateEmployeeName(name);
         this.name = name;
     }
 
     public String getName() {
         return name;
+    }
+
+    private void validateEmployeeName(String name) {
+        if (name.length() > 5) {
+            throw new MyIllegalArgumentException(ErrorMessage.TOO_LONG_EMPLOYEE_NAME);
+        }
     }
 
     @Override
